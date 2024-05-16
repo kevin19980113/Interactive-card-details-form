bbe4dc1b6ab864261fdf83d428fa9869177a7796
HYUNHO <jeny95kr@gmail.com>
2024-05-16 12:34:44 -0400

    final-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import bgImg from "./assets/bg-main-desktop.png";
+import mobileImg from "./assets/bg-main-mobile.png";
 import CardDetailForm from "./components/CardDetailForm";
 import CardFront from "./components/CardFront";
 import CardBack from "./components/CardBack";
 import CardDetailContextProvider from "./cardInfoContext/CardDetailContext.jsx";
+import { useMediaQuery } from "react-responsive";
 
 function App() {
+  const isDesktop = useMediaQuery({ minWidth: "1024px" });
+
   return (
     <main>
       <CardDetailContextProvider>
         <CardFront />
         <CardBack />
-        <img src={bgImg} className="backgroundImg" alt="background image" />
+        <img
+          src={isDesktop ? bgImg : mobileImg}
+          className="backgroundImg"
+          alt="background image"
+        />
         <div className="CardDetailWrapper">
           <CardDetailForm />
         </div>
